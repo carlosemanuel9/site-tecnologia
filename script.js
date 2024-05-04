f86cc726bcbd850344f8e478be6470bea94231b6
Carlos Emanuel Rodrigues Queiroz <166170931+carlosemanuel9@users.noreply.github.com>
2024-05-04 14:28:40 -0300

    new

---
+++ script.js
@@ -0,0 +1,24 @@
+ScrollReveal().reveal('.menu-img' , { 
+    origin:'top',
+    duration : 2000,
+    distance: '100%',
+    reset:'false'
+    });
+    ScrollReveal().reveal('.conteudo' , { 
+        origin:'left',
+        duration : 2000,
+        distance: '100%',
+        reset:'false'
+        });
+        ScrollReveal().reveal('.img-2' , { 
+            origin:'left',
+            duration : 2000,
+            distance: '100%',
+            reset:'false'
+            });
+            ScrollReveal().reveal('.produtos' , { 
+                origin:'bottom',
+                duration : 2000,
+                distance: '100%',
+                reset:'false'
+                });
